ffd573b52c7259f49b5f6fee538265c4153304dd
sgSillage <51256576+sgSillage@users.noreply.github.com>
2020-09-12 16:15:57 +0800

    登陆注册小改

--- 酒店管理系统/FindPassword.Designer.cs
+++ 酒店管理系统/FindPassword.Designer.cs
@@ -30,6 +30,15 @@
         {
             this.buttonFindPCancel = new System.Windows.Forms.Button();
             this.buttonSet = new System.Windows.Forms.Button();
+            this.labelAccount = new System.Windows.Forms.Label();
+            this.labelNewPassword = new System.Windows.Forms.Label();
+            this.labelNewPassword_re = new System.Windows.Forms.Label();
+            this.labelIdentity = new System.Windows.Forms.Label();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.textBoxIdentity = new System.Windows.Forms.TextBox();
+            this.textBoxNewPassword = new System.Windows.Forms.TextBox();
+            this.textBoxNewPassword_re = new System.Windows.Forms.TextBox();
+            this.labelRemain = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonFindPCancel
@@ -48,20 +57,105 @@
             this.buttonSet.Name = "buttonSet";
             this.buttonSet.Size = new System.Drawing.Size(102, 37);
             this.buttonSet.TabIndex = 16;
-            this.buttonSet.Text = "查询";
+            this.buttonSet.Text = "修改";
             this.buttonSet.UseVisualStyleBackColor = true;
+            this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
+            // 
+            // labelAccount
+            // 
+            this.labelAccount.AutoSize = true;
+            this.labelAccount.Location = new System.Drawing.Point(101, 89);
+            this.labelAccount.Name = "labelAccount";
+            this.labelAccount.Size = new System.Drawing.Size(187, 15);
+            this.labelAccount.TabIndex = 18;
+            this.labelAccount.Text = "请输入要找回密码的账号：\r\n";
+            // 
+            // labelNewPassword
+            // 
+            this.labelNewPassword.AutoSize = true;
+            this.labelNewPassword.Location = new System.Drawing.Point(176, 187);
+            this.labelNewPassword.Name = "labelNewPassword";
+            this.labelNewPassword.Size = new System.Drawing.Size(112, 15);
+            this.labelNewPassword.TabIndex = 19;
+            this.labelNewPassword.Text = "重置后的密码：";
+            // 
+            // labelNewPassword_re
+            // 
+            this.labelNewPassword_re.AutoSize = true;
+            this.labelNewPassword_re.Location = new System.Drawing.Point(161, 239);
+            this.labelNewPassword_re.Name = "labelNewPassword_re";
+            this.labelNewPassword_re.Size = new System.Drawing.Size(127, 15);
+            this.labelNewPassword_re.TabIndex = 20;
+            this.labelNewPassword_re.Text = "重新输入新密码：";
+            // 
+            // labelIdentity
+            // 
+            this.labelIdentity.AutoSize = true;
+            this.labelIdentity.Location = new System.Drawing.Point(191, 138);
+            this.labelIdentity.Name = "labelIdentity";
+            this.labelIdentity.Size = new System.Drawing.Size(97, 15);
+            this.labelIdentity.TabIndex = 21;
+            this.labelIdentity.Text = "身份证号码：";
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Location = new System.Drawing.Point(313, 86);
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(260, 25);
+            this.textBoxAccount.TabIndex = 22;
+            // 
+            // textBoxIdentity
+            // 
+            this.textBoxIdentity.Location = new System.Drawing.Point(313, 135);
+            this.textBoxIdentity.Name = "textBoxIdentity";
+            this.textBoxIdentity.Size = new System.Drawing.Size(260, 25);
+            this.textBoxIdentity.TabIndex = 23;
+            // 
+            // textBoxNewPassword
+            // 
+            this.textBoxNewPassword.Location = new System.Drawing.Point(313, 184);
+            this.textBoxNewPassword.Name = "textBoxNewPassword";
+            this.textBoxNewPassword.Size = new System.Drawing.Size(260, 25);
+            this.textBoxNewPassword.TabIndex = 24;
+            // 
+            // textBoxNewPassword_re
+            // 
+            this.textBoxNewPassword_re.Location = new System.Drawing.Point(313, 236);
+            this.textBoxNewPassword_re.Name = "textBoxNewPassword_re";
+            this.textBoxNewPassword_re.Size = new System.Drawing.Size(260, 25);
+            this.textBoxNewPassword_re.TabIndex = 25;
+            // 
+            // labelRemain
+            // 
+            this.labelRemain.AutoSize = true;
+            this.labelRemain.ForeColor = System.Drawing.Color.Red;
+            this.labelRemain.Location = new System.Drawing.Point(310, 212);
+            this.labelRemain.Name = "labelRemain";
+            this.labelRemain.Size = new System.Drawing.Size(175, 15);
+            this.labelRemain.TabIndex = 26;
+            this.labelRemain.Text = "*:请输入9~20位的密码！";
             // 
             // FindPassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelRemain);
+            this.Controls.Add(this.textBoxNewPassword_re);
+            this.Controls.Add(this.textBoxNewPassword);
+            this.Controls.Add(this.textBoxIdentity);
+            this.Controls.Add(this.textBoxAccount);
+            this.Controls.Add(this.labelIdentity);
+            this.Controls.Add(this.labelNewPassword_re);
+            this.Controls.Add(this.labelNewPassword);
+            this.Controls.Add(this.labelAccount);
             this.Controls.Add(this.buttonFindPCancel);
             this.Controls.Add(this.buttonSet);
             this.Name = "FindPassword";
             this.Text = "找回密码";
             this.Load += new System.EventHandler(this.FindPassword_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +163,14 @@
 
         private System.Windows.Forms.Button buttonFindPCancel;
         private System.Windows.Forms.Button buttonSet;
+        private System.Windows.Forms.Label labelAccount;
+        private System.Windows.Forms.Label labelNewPassword;
+        private System.Windows.Forms.Label labelNewPassword_re;
+        private System.Windows.Forms.Label labelIdentity;
+        private System.Windows.Forms.TextBox textBoxAccount;
+        private System.Windows.Forms.TextBox textBoxIdentity;
+        private System.Windows.Forms.TextBox textBoxNewPassword;
+        private System.Windows.Forms.TextBox textBoxNewPassword_re;
+        private System.Windows.Forms.Label labelRemain;
     }
 }
--- 酒店管理系统/FindPassword.cs
+++ 酒店管理系统/FindPassword.cs
@@ -27,5 +27,47 @@ namespace LoginRegisterFrame
             this.Hide();
             this.Owner.Show();
         }
+
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+            string account = textBoxAccount.Text;
+            string identity = textBoxIdentity.Text;
+            string Newpassword = textBoxNewPassword.Text;
+            string NewPassword_re = textBoxNewPassword_re.Text;
+
+            if(account == "")
+            {
+                MessageBox.Show("请输入账号！", "");
+                return;
+            }
+            else if(identity == "")
+            {
+                MessageBox.Show("请输入身份证号码！", "");
+                return;
+            }
+            else if (Newpassword == "")
+            {
+                MessageBox.Show("请输入新密码！", "");
+                return;
+            }
+            else if(NewPassword_re == "")
+            {
+                MessageBox.Show("请输入两次新密码！", "");
+                return;
+            }
+            else if(Newpassword != NewPassword_re)
+            {
+                MessageBox.Show("两次输入的密码不一样，请重新输入!", "");
+                return;
+            }
+            else if(Newpassword.Length >20 || Newpassword.Length < 9)
+            {
+                MessageBox.Show("密码长度不正确，请重新输入！", "");
+                return;
+            }
+
+
+
+        }
     }
 }
